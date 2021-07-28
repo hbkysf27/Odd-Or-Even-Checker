6f7375b9f6b326ea6be3409c90356dfb2e520a41
JT <JT@JT-COMPUTERS>
2021-07-28 19:26:35 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace WinFormsAppEx4Lect8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtinput = new System.Windows.Forms.TextBox();
+            this.btncheck = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(32, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter A value";
+            // 
+            // txtinput
+            // 
+            this.txtinput.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtinput.Location = new System.Drawing.Point(228, 202);
+            this.txtinput.Name = "txtinput";
+            this.txtinput.Size = new System.Drawing.Size(175, 23);
+            this.txtinput.TabIndex = 1;
+            // 
+            // btncheck
+            // 
+            this.btncheck.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btncheck.Font = new System.Drawing.Font("Sylfaen", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.btncheck.Location = new System.Drawing.Point(165, 314);
+            this.btncheck.Name = "btncheck";
+            this.btncheck.Size = new System.Drawing.Size(118, 63);
+            this.btncheck.TabIndex = 2;
+            this.btncheck.Text = "Check";
+            this.btncheck.UseVisualStyleBackColor = true;
+            this.btncheck.Click += new System.EventHandler(this.btncheck_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tw Cen MT Condensed", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label2.Location = new System.Drawing.Point(135, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(190, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Even Or Odd Checker";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 538);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btncheck);
+            this.Controls.Add(this.txtinput);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtinput;
+        private System.Windows.Forms.Button btncheck;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsAppEx4Lect8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncheck_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (oddoreven() == 0)
+                {
+                    MessageBox.Show(txtinput.Text+" is a Even Number ");
+                }
+                else if(oddoreven()==1)
+                {
+                    MessageBox.Show(txtinput.Text+" is a odd Number");
+                }            
+            }
+            catch
+            {
+                
+                    {
+                    MessageBox.Show("invalid number");
+                }
+
+            }
+    
+        }
+        public int oddoreven()
+        {
+            int number;
+            number = int.Parse(txtinput.Text);
+            if(number%2==0)
+            {
+                return 0;
+
+            }
+            else
+            {
+                return 1;
+            }
+
+        }
+    }
+}
